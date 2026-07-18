048e3aaf0ce83398503cd6725530c271d4b2c8eb
Benjamin Gluzman <benjamin.gluzman@gmail.com>
2026-07-18 18:36:52 -0400

    Implement error checking for chrome.storage functions

--- extension/src/storage.js
+++ extension/src/storage.js
@@ -33,27 +33,48 @@ async function initializeStorage() {
 async function getCourseIndexMap() {
     return new Map(
         Object.entries(
-            (await localStorage.get("courseIndexMap"))
-            .courseIndexMap
+            await retry(async () => (await localStorage.get("courseIndexMap")).courseIndexMap, "getCourseIndexMap()")
         )
     );
 }
 
 async function getWatches() {
-    return (await localStorage.get("watches")).watches;
+    return await retry(async () => (await localStorage.get("watches")).watches, "getWatches()")
 }
 
 async function addWatch(courseIndex) {
-    const watches = (await localStorage.get("watches")).watches;
+    const watches = await retry(async () => (await localStorage.get("watches")).watches, "addWatch()");
     watches.push(courseIndex);
 
-    await localStorage.set({ watches });
+    await retry(async () => await localStorage.set({ watches }), "addWatch()");
 }
 
 async function removeWatch(courseIndex) {
-    const watches = (await localStorage.get("watches")).watches;
+    const watches = await retry(async () => (await localStorage.get("watches")).watches, "removeWatch()");
 
-    await localStorage.set({ watches: watches.filter(index => index != courseIndex) });
+    await retry(async () => await localStorage.set({ watches: watches.filter(index => index != courseIndex) }), "removeWatch()");
+}
+
+async function retry(operation, name) {
+    const MAX_RETRIES = 3, RETRY_DELAY = 100;
+
+    for (let attempt = 1; ; attempt++) {
+        try {
+            return await operation();
+        }
+        catch (err) {
+            if (attempt === MAX_RETRIES) {
+                console.error(`${name} failed after ${attempt} attempts`, err);
+                throw err;
+            }
+
+            console.warn(`${name} failed (attempt ${attempt}), retrying...`);
+
+            await new Promise(resolve =>
+                setTimeout(resolve, RETRY_DELAY * attempt)
+            );
+        }
+    }
 }
 
 
